39b7b9996b21c4c6bc415403a6526c2e8d9e7048
unknown <incontextseo@gmail.com>
2019-05-30 23:49:43 -0500

    Changed the VoteSmart endpoint to https

--- assets/js/app.js
+++ assets/js/app.js
@@ -308,7 +308,7 @@ console.log("hello", response)
             "&language=en&sources=fox-news,cnn";
         console.log(candidateQueryURL)
         // Create the return object
-        VSQueryURLD = "http://api.votesmart.org/CandidateBio.getDetailedBio?key=" + votesSmartKey + "&candidateId=" + candidateId + "&o=JSON";
+        VSQueryURLD = "https://api.votesmart.org/CandidateBio.getDetailedBio?key=" + votesSmartKey + "&candidateId=" + candidateId + "&o=JSON";
 
         // wikipedia Variables
         var wikiURL = "https://en.wikipedia.org/api/rest_v1/page/summary/";
@@ -367,52 +367,6 @@ console.log("hello", response)
             requestNews(candidateQueryURL)
 
         });
-
-        // Modified version from here: http://davidwalsh.name/convert-xml-json
-        // function xmlToJson(xml) {
-
-        //     // Create the return object
-        //     var obj = {};
-
-        //     if (xml.nodeType == 1) { // element
-        //         // do attributes
-        //         if (xml.attributes.length > 0) {
-        //             obj["@attributes"] = {};
-        //             for (var j = 0; j < xml.attributes.length; j++) {
-        //                 var attribute = xml.attributes.item(j);
-        //                 obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-        //             }
-        //         }
-        //     } else if (xml.nodeType == 3) { // text
-        //         obj = xml.nodeValue;
-        //     }
-
-        //     // do children
-        //     // If just one text node inside
-        //     if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
-        //         obj = xml.childNodes[0].nodeValue;
-        //     } else if (xml.hasChildNodes()) {
-        //         for (var i = 0; i < xml.childNodes.length; i++) {
-        //             var item = xml.childNodes.item(i);
-        //             var nodeName = item.nodeName;
-        //             if (typeof (obj[nodeName]) == "undefined") {
-        //                 obj[nodeName] = xmlToJson(item);
-        //             } else {
-        //                 if (typeof (obj[nodeName].push) == "undefined") {
-        //                     var old = obj[nodeName];
-        //                     obj[nodeName] = [];
-        //                     obj[nodeName].push(old);
-        //                 }
-        //                 obj[nodeName].push(xmlToJson(item));
-        //             }
-        //         }
-        //     }
-        //     return obj;
-        // };
-
-        // not sure where to put this to hide the card
-        // $(".can-menu").addClass(["card-hidden"]);
-
 
     });
 
